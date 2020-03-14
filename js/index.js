f260b6e05cbe360726632eee4d01052ae782f8f6
Adam-Young <awy6448@gmail.com>
2020-03-13 21:48:12 -0400

    completed the rest of the event listeners and created code for the event propagation and preventDefault.

--- js/index.js
+++ js/index.js
@@ -29,10 +29,54 @@ const body = document.querySelector('body');
 
 letGo.addEventListener('contextmenu', () => {
     body.style.backgroundImage= "url('https://images.unsplash.com/photo-1566577134624-6f6cc4bb272b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80')";
-    console.log('triggered')
 })
 
 body.addEventListener('keydown', () => {
     body.style.backgroundImage= "url('none')"
-    console.log('triggered2')
+})
+
+const middleImgs = document.querySelectorAll('.img-content');
+
+middleImgs[0].addEventListener('wheel', (event) => {
+    event.target.style.transform = 'rotate(45deg)'
+})
+
+const bottomImg = document.querySelector('.content-destination img');
+
+bottomImg.addEventListener('drag', () => {
+    bottomImg.style.display = 'none'
+})
+
+const bottomContent = document.querySelectorAll('.content-pick div');
+
+
+bottomContent.forEach((cb) => {
+    cb.addEventListener('pointerleave', () => {
+        cb.style.border = '5px solid red'
+
+    } )
+})
+
+// part 2 of work
+//stopping event propagation
+body.addEventListener('click', () => {
+    body.style.backgroundColor = 'orange'
+})
+
+const conHome = document.querySelector('.home')
+console.log(conHome)
+
+conHome.addEventListener('click', (event) => {
+    event.target.style.backgroundColor = 'blue'
+    event.stopPropagation()
+})
+
+//preventDefault
+
+const navLinks = document.querySelectorAll('a');
+console.log(navLinks)
+navLinks.forEach(link => {
+    link.addEventListener('click', (event) => {
+        event.preventDefault();
+    })
 })
